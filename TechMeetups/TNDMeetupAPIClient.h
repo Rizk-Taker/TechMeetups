38c5eb93a2404c701725a55d4db728698d739aa2
nrizk6 <nrizk6@gmail.com>
2015-05-07 19:25:33 -0400

    create TNDMeetupAPIClient Class

---
+++ TechMeetups/TNDMeetupAPIClient.h
@@ -0,0 +1,13 @@
+//
+//  TNDMeetupAPIClient.h
+//  TechMeetups
+//
+//  Created by Nicolas Rizk on 5/7/15.
+//  Copyright (c) 2015 Nicolas Rizk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TNDMeetupAPIClient : NSObject
+
+@end
